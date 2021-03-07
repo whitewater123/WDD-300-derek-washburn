e05c62b03c2f381662cc5f9aeafa6fe0312c9f11
whitewater123 <derekwashburn@hotmail.com>
2021-03-06 19:41:27 -0700

    Week 9

--- js/main.js
+++ js/main.js
@@ -9,17 +9,56 @@ const links = [
         url: "week2/w02TeamActivity.html",
     },
     {
+        label: "Week3 Notes",
+        url: "week3/w03Exercises.html"
+        
+    },
+    {
         label: "Week 3 Team Activity",
         url: "week3/w03TeamActivity.html",
+    },
+    {
+        label: "Week4 Notes",
+        url: "week4/w04Examples.html"
+        
     },
     {
         label: "Week 4 Team Activity",
         url: "week4/w04TeamActivity.html",
     },
     {
+        label: "Week5 Notes",
+        url: "week5/w05Exercises.html"
+        
+    },
+    {
         label: "Week 5 Team Activity",
         url: "week5/w05TeamActivity.html",
-    }
+    },
+    {
+        label: "Week7 Notes",
+        url: "week7/w07Exercises.html"
+        
+    },
+    {
+        label: "ToDo App",
+        url: "todo-application/index.html"
+        
+    },
+    {
+        label: "Week8 Notes",
+        url: "week8/w08Examples.html"
+        
+    },
+    {
+        label: "Week 8 Team Activity",
+        url: "week8/w08TeamActivity.html",
+    },
+    {
+        label: "Week 9 Team Activity",
+        url: "week9/index-START.html",
+    },
+    
 ]
 
 const ol = document.getElementById("contents");
